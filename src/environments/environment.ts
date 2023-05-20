2a8956374da37ad78b519a9cb99ee8f1c6d5bfa9
MuhanadTaha <muhannad.oqba@gmail.com>
2023-05-20 16:02:26 +0300

    FirstCom

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    Production:false,
+    weatherApiBaseUrl:'https://weatherapi-com.p.rapidapi.com/current.json',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
+    XRapidAPIKeyHeaderName:  'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: 'c9bfa69b5cmsh877cdcae8e5ae42p1ad311jsn7675fae78ec0'
+};
